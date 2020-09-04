7737613f63de24ed581fbc624947423177acca7f
Michael Snoyman <michael@snoyman.com>
2020-09-04 13:10:15 +0300

    Better host binding

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ async fn index(data: web::Data<AppState>) -> String {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let host = "0.0.0.0:8080";
+    println!("Trying to listen on {}", host);
     let app_state = web::Data::new(AppState {
         counter: Mutex::new(0),
     });
@@ -21,5 +23,5 @@ async fn main() -> std::io::Result<()> {
         App::new()
         .app_data(app_state.clone())
         .service(index)
-    }).bind("127.0.0.1:8080")?.run().await
+    }).bind(host)?.run().await
 }
